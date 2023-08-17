7c924ba505712d589db26c78b1fbdc02bd2bf5bd
Manish Chand <manishchandr349@gmail.com>
2023-08-17 12:57:55 +0530

    adding

---
+++ index.js
@@ -0,0 +1,31 @@
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+const digitalTime = document.getElementById("digital-time");
+const showTimeButton = document.getElementById("show-time-btn");
+
+ function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const mins = now.getMinutes();
+  const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hour = now.getHours();
+  const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  function showtime(){
+    digitalTime.textContent = now.toLocaleTimeString();
+  
+  }
+  showTimeButton.addEventListener("click", () => {
+    showtime();
+  });
+}
+
+setDate();
+setInterval(setDate, 1000);
